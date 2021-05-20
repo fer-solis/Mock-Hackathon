3d983327224ac825c149757671df23875bb34f7b
MiriamMVentura <miriamartv@gmail.com>
2021-05-20 18:29:19 -0500

    add firebase

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
 import './App.css';
+import  {db} from './firebase';
+import React, {useState, useEffect} from 'react'
+
+
 
 function App() {
+
+//   const [getProducts, setGetProducts] = useState([]);
+
+//   useEffect(() => {
+//     const getProducts = () =>{
+//     const { docs } =  db.collection('Productos').doc('VESTIDOS').get();
+//     const nuevoArray = docs.map(item =>({name: item.Nombre, ...item.data() }))
+//     setGetProducts(nuevoArray)
+//     console.log(nuevoArray); //renderiza las notas desps de tomar la info
+//   }
+//   getProducts(); //trae los datos desde firebase y ademas los renderiza y tmbn los ids pa usar dsps.
+// },[])
+
+// REFERENCIA A LA BASE DE DATOS
+  const productColection = db.collection('Productos').doc('VESTIDOS');
+
+  // TREAMOS LA DATA DE LA REFERENCIA A LA CONSOLA
+  productColection.get().then((doc) => {
+      if (doc.exists) {
+          console.log("Document data:", doc.data());
+      } else {
+          // doc.data() will be undefined in this case
+          console.log("No such document!");
+      }
+  }).catch((error) => {
+      console.log("Error getting document:", error);
+  });
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 key={doc.data('Nombre')}></h1>
+      {/* <p id='name'>{doc.data(Nombre)}</p> */}
     </div>
   );
 }
---
+++ src/categories.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function categories() {
+
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+export default categories
