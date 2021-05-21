10f10eb14cddadff4ee7b40301ebc3626232711e
MiriamMVentura <miriamartv@gmail.com>
2021-05-21 01:27:23 -0500

    add create product, form product, add banners

--- src/AdminProducts.jsx
+++ src/AdminProducts.jsx
@@ -1,121 +1,111 @@
-import React from 'react';
+import React, { Fragment } from 'react';
+import { db } from './firebase';
 import { useEffect, useState } from 'react';
 
-import { db } from './firebase';
+import CrudProduct from './components/CrudProduct'
 
-
-
-
-export default function AdminProducts() {
-
-  const [ productList, setproductList ] = useState([])
-  const [ name, setName ] = useState('')
-  const [ description, sepescription ] = useState('')
-  const [ color, setColor ] = useState('')
-  const [ price, setPrice] = useState('')
-  const [ category, setCategory] = useState('')
-  const [ type, setType] = useState('')
-  const [ image, setImage] = useState('')
+export default function AdminProducts(props) {
+  const [productList, setProductList] = useState([]);
+  const [name, setName] = useState("");
+  const [description, sepescription] = useState("");
+  const [color, setColor] = useState("");
+  const [price, setPrice] = useState("");
+  const [category, setCategory] = useState("");
+  const [type, setType] = useState("");
+  const [image, setImage] = useState("");
 
   useEffect(() => {
-    getProducts()
-  }, [])
+    getProducts();
+  }, []);
 
   const getProducts = async () => {
-    let obj;
-    let list = []
-    const querySnapshot = await db.collection('products').get();
-    querySnapshot.forEach((doc) => {
-      obj = doc.data()
-      obj.id = doc.id
-      list.push(obj)
+    // onSnapshot hace un seguimineto en tiempo real
+    db.collection("products").onSnapshot((querySnapshot) => {
+      const docs = [];
+      querySnapshot.forEach((doc) => {
+        docs.push({ ...doc.data(), id: doc.id });
+      });
+      // añadimos todos los productos al estado
+      setProductList(docs);
     });
-    setproductList(list)
-  }
+  };
 
-  const addProduct = async() => {
-    const obj = {name, description, color, price, category, type, image }
-    const dbRef = await db.collection('products').add(obj)
-    // console.log(dbRef.id)
-    clearInput()
-    getProducts()
-
-  }
+  const addProduct = async () => {
+    const obj = { name, description, color, price, category, type, image };
+    await db.collection("products").add(obj);
+    clearInput();
+    getProducts();
+  };
 
   const clearInput = () => {
-    setName('')
-    sepescription('')
-    setColor('')
-    setPrice('')
-    setCategory('')
-    setType('')
-    setImage('')
-  }
-
+    setName("");
+    sepescription("");
+    setColor("");
+    setPrice("");
+    setCategory("");
+    setType("");
+    setImage("");
+  };
 
   return (
-    <div div className="App">
-      <input
-        className="form-control"
-        placeholder="Nombre"
-        onChange={(e) => setName(e.target.value)}
-        value={name}
-      />
-      <input
-        className="form-control"
-        placeholder="Description"
-        onChange={(e) => sepescription(e.target.value)}
-        value={description}
-      />
-      <input
-        className="form-control"
-        placeholder="Color"
-        onChange={(e) => setColor(e.target.value)}
-        value={color}
-      />
-      <input
-        className="form-control"
-        placeholder="Price"
-        onChange={(e) => setPrice(e.target.value)}
-        value={price}
-      />
-      <input
-        className="form-control"
-        placeholder="Categoría"
-        onChange={(e) => setCategory(e.target.value)}
-        value={category}
-      />
-      <input
-        className="form-control"
-        placeholder="Tipo?"
-        onChange={(e) => setType(e.target.value)}
-        value={type}
-      />
-      <input
-        className="form-control"
-        placeholder="Imagen url"
-        onChange={(e) => setImage(e.target.value)}
-        value={image}
-      />
-      <button className="btn-primary" onClick={addProduct}>
-        Agregar Producto
-      </button>
+    <div className="crud-content">
+      <Fragment>
+        <input
+          className="form-control"
+          placeholder="Nombre"
+          onChange={(e) => setName(e.target.value)}
+          value={name}
+        />
+        <input
+          className="form-control"
+          placeholder="Description"
+          onChange={(e) => sepescription(e.target.value)}
+          value={description}
+        />
+        <input
+          className="form-control"
+          placeholder="Color"
+          onChange={(e) => setColor(e.target.value)}
+          value={color}
+        />
+        <input
+          className="form-control"
+          placeholder="Price"
+          onChange={(e) => setPrice(e.target.value)}
+          value={price}
+        />
+        <input
+          className="form-control"
+          placeholder="Categoría"
+          onChange={(e) => setCategory(e.target.value)}
+          value={category}
+        />
+        <input
+          className="form-control"
+          placeholder="Tipo?"
+          onChange={(e) => setType(e.target.value)}
+          value={type}
+        />
+        <input
+          className="form-control"
+          placeholder="Imagen url"
+          onChange={(e) => setImage(e.target.value)}
+          value={image}
+        />
+        <button className="btn-primary" onClick={addProduct}>
+          Agregar Producto
+        </button>
 
-      <table className="table">
-        {productList.map((product, index) => (
-          <tr key={index}>
-            <p>{index + 1}</p>
-            <p>{product.id}</p>
-            <p>{product.name}</p>
-            <p>{product.color}</p>
-            <p>{product.description}</p>
-            <p>{product.price}</p>
-            <p>{product.type}</p>
-            <p>{product.category}</p>
-            <img src={product.image} id="product-photos"></img>
-          </tr>
-        ))}
-      </table>
+        {productList &&
+          productList.map((product) => (
+            <CrudProduct
+              key={product.id}
+              product={product}
+              productList={productList}
+              setProductList={setProductList}
+            />
+          ))}
+      </Fragment>
     </div>
   );
 }
---
+++ src/components/Search.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Search () {
+    return (
+    <div><h1>Buscar</h1>
+    <i class="material-icons">zoom_in</i></div>);
+  }
+
+  export default Search;
