ec3c3a7ac6cda7a43f62e2c0a48a2d3490011250
FernandaSolis <fersolis518@gmail.com>
2021-05-21 12:41:26 -0500

    btns a products

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -4,14 +4,28 @@ import Group1 from "../assets/Group1.png";
 import Group2 from "../assets/Group2.png";
 import Group3 from "../assets/Group3.png";
 import Group4 from "../assets/Group4.png";
+import { useHistory } from "react-router-dom";
 
 const Table = () => {
+  const history = useHistory();
+
+  const goProducts = () => {
+    history.push("/products");
+  };
   return (
     <div>
-      <img src={Group1} />
-      <img src={Group2} />
-      <img src={Group3} />
-      <img src={Group4} />
+      <button className="btn" onClick={goProducts}>
+        <img src={Group1} />
+      </button>
+      <button className="btn" onClick={goProducts}>
+        <img src={Group2} />
+      </button>
+      <button className="btn" onClick={goProducts}>
+        <img src={Group3} />
+      </button>
+      <button className="btn" onClick={goProducts}>
+        <img src={Group4} />
+      </button>
     </div>
   );
 };
